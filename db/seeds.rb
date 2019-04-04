4943a377ff3c170d0512bd014aa45bdf8ce1fe2a
studioshane <studioshane@gmail.com>
2019-04-04 10:10:06 +0100

    seed data all done

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,15 +12,21 @@ def t_or_f
 [true, false].sample
 end
 
+def full_name 
+  "#{Faker::Name.first_name} #{Faker::Name.last_name}"
+end 
+
 100.times do 
   User.create(email: Faker::Internet.email, password: "password")
 end 
 
+
+
 def generate_technician
     num = 1 
     until num == 50
-    day_rate = [200, 225, 250, 255, 260, 275]
-    Technician.create(user_id: num, name: Faker::Name.name, email:User.all[num-1].email, phone: Faker::PhoneNumber.cell_phone,  day_rate: day_rate, audio: t_or_f, video: t_or_f, lighting: t_or_f, general: t_or_f)
+    day_rate = [200, 225, 250, 255, 260, 275].sample
+    Technician.create(user_id: num, name: full_name, email:User.all[num-1].email, phone: Faker::PhoneNumber.cell_phone,  day_rate: day_rate, audio: t_or_f, video: t_or_f, lighting: t_or_f, general: t_or_f)
     num += 1
     end 
 end 
@@ -29,7 +35,7 @@ def generate_producer
     num = 50
     until num == 101
     company = ["PSAV", "AVC Live", "PSPAV", "Blitz", "Present Communications", "Waters Productions", "Eclipse Presentations", "Metro Broadcast", "Aztec", "Creative Staging", "Dreamtek", "Electrosonic", "First Network", "GEMS", "Icon", "MCL" ].sample
-    Producer.create(user_id: num, name: Faker::Name.name, email:User.all[num-1].email, phone: Faker::PhoneNumber.cell_phone, company: company)
+    Producer.create(user_id: num, name: full_name, email:User.all[num-1].email, phone: Faker::PhoneNumber.cell_phone, company: company)
     num += 1
     end 
 end 
@@ -62,13 +68,8 @@ end
 
 def create_event
 num = gen_random_num(1)
-
-    case num 
-    when 0 
         company = "#{Faker::Company.name} #{Faker::Company.suffix}"
-    when 1
-        company = Faker::Company.industry
-    end 
+  
     venue = ["St. Pancras Renaissance", "Royal Garden Hotel", "Shell Bulding", "ExCel London", "Olympia London", "Barclays 1CP", "Corinthia Hotel", "Glaziers Hall", "Drapers Hall", "Royal Tobacco Dock", "The Brewery", "The Truman Brewery", "Institute of Directors", "Natural History Museum", "The Kia Oval", "The Crystal", "The Savoy", "ETC St. Paul's", "ETC County Hall", "InterContinental 02", "InterContinental Park Lane", "Grosvenor Hotel", "Hilton Metropole", "Hilton Paddington"].sample
  Event.create(producer: Producer.all.sample, venue: venue, start_date: dates[0], end_date:dates[0], techs_required: (1..3).to_a.sample, client: company)
 end 
@@ -82,3 +83,16 @@ generate_producer
 50.times do
     create_event
 end
+
+def add_techs_to_events
+    Event.all.each do |event|
+        rand(event.techs_required + 1).times do
+            event.technicians << Technician.all.sample
+        end
+    end 
+
+end 
+
+add_techs_to_events
+
+
